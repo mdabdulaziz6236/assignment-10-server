1e59419b28b64256ac4268a26e790029a0c209aa
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-09 16:35:09 +0600

    In the Name of Allah, Server started with Express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Server is running Fine.')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
